06741b5d0bedc6f7c24f71e8e178e5a0ceb287de
mbisurgisoshace <maximiliano.bisurgi@gmail.com>
2024-01-28 23:41:08 -0300

    Add User prisma model and RegisterSchema

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,10 +1,27 @@
 import * as z from "zod";
+import { UserRole } from "@prisma/client";
 
 export const LoginSchema = z.object({
   email: z.string().email({
-    message: "Email is required",
+    message: "Email es requerido",
   }),
   password: z.string().min(1, {
-    message: "Password is required",
+    message: "Password es requerido",
   }),
+});
+
+export const RegisterSchema = z.object({
+  email: z.string().email({
+    message: "Email es requerido",
+  }),
+  password: z.string().min(6, {
+    message: "Minimo de 6 caracteres requeridos",
+  }),
+  nombre: z.string().min(1, {
+    message: "Nombre es requerido",
+  }),
+  apellido: z.string().min(1, {
+    message: "Apellido es requerido",
+  }),
+  role: z.nativeEnum(UserRole).default("USER"),
 });
