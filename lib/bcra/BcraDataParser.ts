80024b6114b41fa3b04b972d5452f1e96f7b100f
Maximiliano Bisurgi <maximiliano.bisurgi@gmail.com>
2025-02-13 14:11:56 -0300

    Comenzar con BcraDataParser y db schema

--- lib/bcra/BcraDataParser.ts
+++ lib/bcra/BcraDataParser.ts
@@ -1,3 +1,37 @@
+interface BcraResultChequesDetalle {
+  nroCheque: number;
+  fechaRechazo: string;
+  monto: number;
+  fechaPago: string;
+  fechaPagoMulta: string;
+  estadoMulta: string;
+  ctaPersonal: boolean;
+  denomJuridica: string;
+  enRevision: boolean;
+  procesoJud: boolean;
+}
+
+interface BcraResultChequesEntidad {
+  entidad: number;
+  detalle: BcraResultChequesDetalle[];
+}
+
+interface BcraResultChequesCausal {
+  causal: string;
+  entidades: BcraResultChequesEntidad[];
+}
+
+interface BcraResultCheques {
+  identificacion: number;
+  denominacion: string;
+  causales: BcraResultChequesCausal[];
+}
+
+export interface BcraResultChequesResponse {
+  status: number;
+  results: BcraResultCheques;
+}
+
 interface BcraResultEntidad {
   entidad: string;
   situacion: number;
@@ -26,4 +60,22 @@ interface BcraResult {
 export interface BcraDataResponse {
   status: number;
   results: BcraResult;
+}
+
+export default class BcraDataParser {
+  private bcraResponse: BcraDataResponse;
+  private bcraHistoricoResponse: BcraDataResponse;
+  private chequesResponse: BcraResultChequesResponse;
+
+  constructor(
+    bcraResponse: BcraDataResponse,
+    bcraHistoricoResponse: BcraDataResponse,
+    chequesResponse: BcraResultChequesResponse
+  ) {
+    this.bcraResponse = bcraResponse;
+    this.chequesResponse = chequesResponse;
+    this.bcraHistoricoResponse = bcraHistoricoResponse;
+  }
+
+  getParsedData() {}
 }
